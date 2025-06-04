586a21582b163d25e7640c08c35af5dfb6774a11
Ahstell <torut1638@gmail.com>
2025-06-04 12:09:25 -0500

    Tipos de datos, concatenar, operadores de pertenecia

--- variables/variables.py
+++ variables/variables.py
@@ -12,3 +12,20 @@ print(nombre)
 numero = 10
 numero += 3
 print(numero)
+
+#Primera forma de concaquetenar con +
+nmbre = "Darkenis"
+bienvenida = "Hola Bienvenida " + nmbre + " Como estas?"
+print(bienvenida)
+
+#Segunda forma de concaquetenar f string
+
+nombre = "Ahstell"
+bienvenida = f"Hola {nombre} como estas"
+print_("Ahstell" in bienvenida)
+
+#Operadores de pertenecnia (in / not in)
+
+print("Lucas" in bienvenida) #true
+
+print("Lucas" not in bienvenida) #false
