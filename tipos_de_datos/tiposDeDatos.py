83639d14c19b27a2cd055a0256669d2f228565d8
Ahstell <torut1638@gmail.com>
2025-06-03 23:31:48 -0500

    se agrego el archivo variables.py y se explico el funcionamiento de una variable

--- tipos_de_datos/tiposDeDatos.py
+++ tipos_de_datos/tiposDeDatos.py
@@ -1,8 +1,15 @@
+
+#Tipo de dato String, con coma y con comillas
 "String"
 'String'
 
 """String"""
 '''String'''
 
+#Tipo de datos numericos enteros y de coma flotante
 4
 1.4
+
+#Tipo de dato Booleano True or False
+False
+True
---
+++ variables/variables.py
@@ -0,0 +1,14 @@
+#Variables en Python
+a = 5
+b = 7
+c = a + b
+print(c)
+
+#Ejemplos de variables
+nombre = "Ahstell"
+print(nombre)
+#La variables se declaran y se definen y son modificables
+
+numero = 10
+numero += 3
+print(numero)
