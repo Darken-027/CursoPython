f29aff13950cf029423e25648af170c46440fb2c
Ahstell <torut1638@gmail.com>
2025-06-05 16:43:36 -0500

    Condicional if-else

---
+++ condicionales/if-else.py
@@ -0,0 +1,7 @@
+#Condicionales
+contraseña_almacenada = "AhstelO27"
+contraseña_escrita = '''Ahstelll_027'''
+if contraseña_almacenada == contraseña_escrita: 
+    print("INICIANDO SESION....")
+else:
+    print("La contraseña que real")
