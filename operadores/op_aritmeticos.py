23d288ec6e1948e050b81f1fd561c73ad44fc21a
Ahstell <torut1638@gmail.com>
2025-06-05 00:55:20 -0500

    Operadores aritmeticos

---
+++ operadores/op_aritmeticos.py
@@ -0,0 +1,19 @@
+#Suma y resta ( + y - )
+suma = 12 + 5
+resta = 24 - 10
+
+#Multiplicaciony division ( * y / )
+multiplicacion = 13 * 66
+division = 3 / 55 #Devuelve el dato de tipo float
+
+#Potenciacion ( exponente (**) )
+exponente = 22 ** 6
+
+#Division baja ( // )
+division_baja = 44 // 90
+
+#Resto o modulo
+resto = 67 % 3
+
+tipo_de_dato = type(division_baja) #type(dato) nos devuelve que tipo de dato es
+print(tipo_de_dato)
