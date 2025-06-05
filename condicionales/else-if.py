4702e61531251af58fa9c3015a37d84f306c2656
Ahstell <torut1638@gmail.com>
2025-06-05 17:59:01 -0500

    operador else-if

---
+++ condicionales/else-if.py
@@ -0,0 +1,16 @@
+ingreso_mensual = 10000
+
+if ingreso_mensual > 1000:
+    print("Estas bien economicamente en cualquierparte del mundo")
+    
+elif ingreso_mensual > 1000:
+    print("Estas bien economicamente en latinoamerica")
+    
+elif ingreso_mensual > 500:
+    print("Estas bien economicamente en colombia")
+    
+elif ingreso_mensual > 200:
+    print("Estas bien economicamente en venezuela")
+    
+else:
+    print("Eres pobre")
