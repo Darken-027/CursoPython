cf4470fb2cca34abe82ae84c520fc990a22d8481
Ahstell <torut1638@gmail.com>
2025-06-04 23:12:10 -0500

    tipo de dato conjunto

--- tipos_de_datos/datos-compuestos.py
+++ tipos_de_datos/datos-compuestos.py
@@ -3,3 +3,17 @@
 #Matris o Lista son conjuntos de Datos
 lista = ["David Ahstell","Soy Ahstell",True,1.70]
 print(lista[0])
+
+#Tupa son iguales a las listas pero no se pueden modificar
+tupla = ("David Ahstell","Soy Ahstell",True,1.70)
+print(tupla[0])
+
+#Creando un conjunto
+#Un conjunto es casi igual que una lista pero no se pueden modificar elementos
+#Se puede reconstruir pero cada elemento no se puede modificar
+#No podemos acceder por el indice a un elemento del conjunto
+#No nos permite repetir valores
+#Se puede iterar
+
+conjunto = {"Ahstell Data", "Soy Ahstell", True, 1.60}
+print(conjunto)
