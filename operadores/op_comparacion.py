737246bee9c7dbcfec6184953ff2e71130b7ca1f
Ahstell <torut1638@gmail.com>
2025-06-05 01:17:53 -0500

    Operadores de comparacion

---
+++ operadores/op_comparacion.py
@@ -0,0 +1,28 @@
+#Operadores de comparacion nos devuelven True o False
+
+igual_que = 5 == 4
+
+distinto_de = 5 != 6
+
+mayor_que = 5 > 6
+
+menor_que = 5 < 6
+
+mayor_o_igual = 5 >= 6    
+
+menor_o_igual = 5 <= 5
+
+print(igual_que)
+print(distinto_de)
+print(mayor_que)
+print(menor_que)
+print(mayor_o_igual)
+print(menor_o_igual)
+
+#Calculos combinados
+
+a = 5
+b = 4
+c = 8
+compracion = a + b < c
+print(compracion)
