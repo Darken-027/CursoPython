c9cf83ba09af20e45e95c99b9ad751c2bfc7f172
Ahstell <torut1638@gmail.com>
2025-06-05 18:18:13 -0500

    Operadores logicos

---
+++ operadores/op_logicos.py
@@ -0,0 +1,18 @@
+#AND
+
+Resultado1 = True & True #Devolver True
+Resultado2 = False & True #Devolver Falso
+Resultado3 = True & False #Devolver Falso
+Resultado4 = False & False #Devolver Falso
+
+#OR
+
+Resultado5 = True | True #Devolver True
+Resultado6 = False | True #Devolver Falso
+Resultado7 = True | False #Devolver Falso
+Resultado8 = False | False #Devolver Falso
+
+#NOT    
+
+Resultado10 = not True #Devolver Falso
+Resultado11 = not False #Devolver Falso
