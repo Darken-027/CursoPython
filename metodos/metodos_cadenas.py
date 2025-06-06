ed850e6dd62a97074b5c77ffb6617350772df7cf
Ahstell <torut1638@gmail.com>
2025-06-05 19:53:45 -0500

    Metodos candenas

---
+++ metodos/metodos_cadenas.py
@@ -0,0 +1,15 @@
+#Los metodos son el DATO.METODO()
+cadena1 = "hola soy Ahstell"
+cadena2 = "como estas"
+
+#conbierte a mayudculas
+mayusc = cadena1.upper()
+
+#conbierte a mayudculas
+minusc = cadena1.lower()
+
+#primera letra en mayuscula
+primera_letra_mayuscula = cadena2.capitalize()
+
+print(primera_letra_mayuscula)
+
