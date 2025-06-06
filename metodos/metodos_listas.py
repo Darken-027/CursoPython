67f3d39441d66531ea50aae81fdb4c955ef8c774
Ahstell <torut1638@gmail.com>
2025-06-05 21:57:23 -0500

    metodos de una lista

---
+++ metodos/metodos_listas.py
@@ -0,0 +1,31 @@
+#creando una lista list()
+lista = list([33,34,55])
+
+#devuelve la cantidad de elementos de la lista
+cantidad_elementos = len(lista)
+
+#grenando un elemento a la lista
+lista.append(88)
+
+#agregando un elemento a la lista con indice especifico
+
+lista.insert(2)
+
+#agregando varios elementos a la lista
+lista.extend([20230])
+
+#eliminando un elemnto de la lista
+print(len(lista))
+
+lista.pop(-1) #-1 para eliminar el ultimo elemento de la lista y asi sucesivamente
+
+#removiendo un elemento de la lista por su valor
+lista.remove(44)
+
+#eliminando elementos de la lista
+#lista.clear
+
+lista.sort
+
+print(lista)
+
