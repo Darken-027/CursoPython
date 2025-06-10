07e1d6c49c50742cd636712569f75d8e2816c057
Ahstell <torut1638@gmail.com>
2025-06-10 16:31:24 -0500

    desenpaquetando datos

---
+++ variables_2.0/desenpaquetado.py
@@ -0,0 +1,9 @@
+#creando datos
+datos = ("Ahstell", "David",1000000)
+datos = ("Ahstell", "David",1000000)
+
+#desenpaquetando
+nombre,apellido,suscriptores = datos
+
+#mostrando resultado
+print(suscriptores)
