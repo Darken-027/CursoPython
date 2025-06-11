04d30255493b4d768eaaa803cd4a2db0329bb02f
Ahstell <torut1638@gmail.com>
2025-06-10 23:09:16 -0500

    variables 2.0 tuplas

---
+++ variables_2.0/tuplas.py
@@ -0,0 +1,10 @@
+#creando tupla con tuple()
+tupla = tuple(["dato1","dato2"])
+
+#creando una tuplaza sin parentisis de mulples datos
+tupla = "dato1", "dato2"
+
+#creando una tupla sin parentesis de un solo dato
+tupla = "dato"
+
+print(tupla)
