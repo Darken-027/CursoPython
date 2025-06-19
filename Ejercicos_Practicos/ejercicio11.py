265d3e0478538ea66d9d05a58d4855852c75bbd6
Ahstell <torut1638@gmail.com>
2025-06-19 15:50:15 -0500

    Ejercicio #11 calcular el area de un rectangulo pidiendo la base y la altura al usuario

---
+++ Ejercicos_Practicos/ejercicio11.py
@@ -0,0 +1,12 @@
+'''
+Ejercicio 11
+calcula el area de un rectangulo
+pide base y altura al usuario  
+
+'''
+base = int(input("Ingresa base "))
+altura = int(input("Ingresa altura "))
+
+area = base * altura
+
+print("Area = " ,area)
