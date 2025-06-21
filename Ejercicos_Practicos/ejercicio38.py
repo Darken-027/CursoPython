01aa1ee1a1429d78bdcbf794e434e90cfacd78dc
Ahstell <torut1638@gmail.com>
2025-06-21 17:54:10 -0500

    Ejercicio #38 Determinando si un numero es divisible entre 5 y 7

---
+++ Ejercicos_Practicos/ejercicio38.py
@@ -0,0 +1,12 @@
+'''
+Ejercicio 38
+determinar si un numero es divisible
+entre 5 y 7
+
+'''
+
+numero = 35
+if numero % 5 == 0 and numero % 7 == 0:
+    print('Es divisible entre 5 y 7')
+else:
+    print('No es divisible entre 5 y 7')
