25819bf57c5dbb3f1c8ca5436dcd099e481a304f
Ahstell <torut1638@gmail.com>
2025-06-21 17:36:29 -0500

    Ejercicio #35 comprovando si un numero esta en el rango de 1 a 100

---
+++ Ejercicos_Practicos/ejercicio35.py
@@ -0,0 +1,11 @@
+'''
+Ejercicio 35
+comprueva si un numero
+esta en el rango de 1 a 100
+
+'''
+numero = 88
+if 1 <= numero <= 100:
+    prin('Esta en el rango de 1 a 100')
+else:
+    print('No esta en el rango')
