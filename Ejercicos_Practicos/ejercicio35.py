55777d4b89b1b28d9d55bd845e8e4f8e110c7084
Ahstell <torut1638@gmail.com>
2025-06-21 17:47:40 -0500

    Ejercicio #37 calculando el maximo de 3 numeros·

--- Ejercicos_Practicos/ejercicio35.py
+++ Ejercicos_Practicos/ejercicio35.py
@@ -6,6 +6,6 @@ esta en el rango de 1 a 100
 '''
 numero = 88
 if 1 <= numero <= 100:
-    prin('Esta en el rango de 1 a 100')
+    print('Esta en el rango de 1 a 100')
 else:
     print('No esta en el rango')
---
+++ Ejercicos_Practicos/ejercicio37.py
@@ -0,0 +1,12 @@
+'''
+Ejercicio 37
+calcular el maximo de 3 numeros
+
+'''
+
+numero1 = 9
+numero2 = 8
+numero3 =5
+
+maximo = max(numero1, numero2, numero3)
+print("El numero max", maximo)
