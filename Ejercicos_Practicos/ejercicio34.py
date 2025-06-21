162c24763420bb6146d8c2a6c30193c5801e6310
Ahstell <torut1638@gmail.com>
2025-06-21 17:28:57 -0500

    Ejercicio #34 verificando si una cadena es mayor o igual a 10 caracteres

---
+++ Ejercicos_Practicos/ejercicio34.py
@@ -0,0 +1,11 @@
+'''
+Ejercicio 34
+verifica si una cadena es mayor
+o igual a 10 caracteres
+
+'''
+cadena = 'Hola que tal'
+if len(cadena) >= 10:
+    print('La cadena tiene 10 o mas caracteres')
+else:
+    print('La cadena tiene menos de 1o caracteres')
