d6a6280cfe10b5985e02767ab2ab0b705a979545
Ahstell <torut1638@gmail.com>
2025-06-21 17:57:50 -0500

    Ejercicio #39 Verificando si la palabra ingresada es python

---
+++ Ejercicos_Practicos/ejercicio39.py
@@ -0,0 +1,11 @@
+'''
+Ejercicio 39
+Verifica si la palabra ingresa es python
+
+'''
+
+palabra = input("Ingres apalabra")
+if palabra.lower() == "python":
+    print('Exito')
+else:
+    print('Error')
