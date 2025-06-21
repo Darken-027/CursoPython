133b8a29e9a9a48292d04e96d33683ca207608e7
Ahstell <torut1638@gmail.com>
2025-06-21 16:49:36 -0500

    Ejercicio #31 pidiendo un numero y verificando si es positivo o negativo o cero

---
+++ Ejercicos_Practicos/ejercicio31.py
@@ -0,0 +1,15 @@
+'''
+Ejercicio 31
+pide un numero y verifica si
+es positivo, negativo o cero
+
+'''
+
+numero = int(input("Escribe un numero"))
+
+if numero > 0:
+    print('Positivo')
+elif numero < 0:
+    print('Negativo')
+else:
+    print('cero')
