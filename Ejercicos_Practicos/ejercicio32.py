03eb795defbbfcff3059ca4651f83300d4a73513
Ahstell <torut1638@gmail.com>
2025-06-21 16:59:38 -0500

    Ejercicio #32 Pidiendo un numero y comprobando si el numero es par o impar utilizando if y modulo

---
+++ Ejercicos_Practicos/ejercicio32.py
@@ -0,0 +1,14 @@
+'''
+Ejercicio 32
+pide un numero y comprueva si 
+el numero es par o impar 
+utilizando if y modulo
+
+'''
+
+numero = int(input('Ingresa un numero'))
+
+if numero % 2 == 0:
+    print('Es un numero par')
+else:
+    print('Es un numero impar')
