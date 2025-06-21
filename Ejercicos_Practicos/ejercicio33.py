aacbbf566a3709e7513ada72c0be8ff2cb973c11
Ahstell <torut1638@gmail.com>
2025-06-21 17:17:45 -0500

    Ejercicio #32 determinando si un año es bisiento

---
+++ Ejercicos_Practicos/ejercicio33.py
@@ -0,0 +1,17 @@
+'''
+Ejercicio 32
+determina si un año es bisiesto
+regla de negocio
+    -divisible por 4
+    -no divisible por 100
+    -divisible por 400
+
+'''
+
+anio = 2025 
+
+if(anio % 4 == 0 and anio% 100 != 0) or \
+    (anio % 400 == 0):
+    print('Es un año bisiesto')
+else:
+    print('No es bisiesto')
