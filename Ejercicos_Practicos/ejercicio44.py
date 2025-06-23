795f5ee5ba7e1867797f40fcf733017da0f7c2d8
Ahstell <torut1638@gmail.com>
2025-06-23 13:54:29 -0500

    Ejercicio 44 Generando un numero aleatorio del 1 al 10 para que el usuario lo adivine hasta que lo haga correctamente

---
+++ Ejercicos_Practicos/ejercicio44.py
@@ -0,0 +1,18 @@
+'''
+Ejercicio 44
+generar un numero aleatorio entre 1 y 10,
+luego, pide al usuario adivinar el numero
+hasta que lo haga correctamente.
+
+'''
+
+import random
+numero_secreto = random.randint(1, 10)
+intentos = 0
+
+while true:
+    intento = int(input('Adivina el numero'))
+    intentos = intentos + 1
+    if intento == numero_secreto:
+        print(f"Exito! tomo {intento} intentos")
+        break
