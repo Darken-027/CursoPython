b6d8a23130372885416e2a2ac90caf9bde407d5a
Ahstell <torut1638@gmail.com>
2025-06-23 14:00:19 -0500

    Ejercicio #45 Imprimiendo la tabla de multiplicar de un numero ingresado por el usuario

---
+++ Ejercicos_Practicos/Ejercicio45.py
@@ -0,0 +1,12 @@
+'''
+Ejercicio 45
+imprime la tabla de multiplicar
+de un numero ingresado por el usuario
+
+'''
+
+numero = int(input('Ingresa el numero'))
+i = 1
+while i <= 10:
+    print(f"{numero} x {i} = {numero * i}")
+    i = i + 1:
